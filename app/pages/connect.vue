1b901584a6d1176eb97eb1dc609d36a4332ab18f
Molokov Sergey <139114308+molsrg@users.noreply.github.com>
2025-06-18 19:36:21 +0300

    Update connect.vue

--- app/pages/connect.vue
+++ app/pages/connect.vue
@@ -185,7 +185,6 @@ const stepper = useTemplateRef<{ hasPrev: boolean }>('stepper')
           <template #qr>
             <UCard variant="subtle" class="animate-fade-in">
               <div class="flex flex-col items-center gap-4">
-                {{ qrUrl }}
                 <QrcodeVue :value="qrUrl" :size="200" />
               </div>
             </UCard>
